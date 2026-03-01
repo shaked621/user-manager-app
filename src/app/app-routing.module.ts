9f82509e40c47a95dfa471d86bfe8075bfa79944
Shaked <Shaked>
2026-03-01 20:41:13 +0200

    Migration to lazy-loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './users/users.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    component: UsersComponent,
+    loadComponent: () => import('./users/users.component').then(m => m.UsersComponent),
   },
 ];
 
