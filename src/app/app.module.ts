bb35b7bc51d0adbb34d0772414f8f7f2aa6bc0bb
Shaked <Shaked>
2026-03-01 15:57:42 +0200

    Migrate to standalone - second step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+
 import { AddUserButtonComponent } from './users/add-user-button/add-user-button.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,14 +13,13 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NoopAnimationsModule,
-        FormsModule,
-        MaterialModule,
-        HttpClientModule,
-        UsersComponent, AddUserButtonComponent,
-    ],
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    UsersComponent, AddUserButtonComponent,
+],
     providers: [],
     bootstrap: [AppComponent],
 })
